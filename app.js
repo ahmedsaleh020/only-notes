198fa5f0930018a5e5899eca8c7d867a77f35553
Ahmed Saleh <ahmed1a1a3a100@gmail.com>
2024-03-27 05:08:23 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,155 @@
+let input = document.querySelector(".user-input");
+let saveBtn = document.querySelector(".saveBtn");
+let noteUpdaterBox = document.querySelector(".note-updater");
+let inputUpdate = document.querySelector(".note-edit-input");
+let updateBtn = document.querySelector(".updateBtn");
+let loginPage = document.querySelector(".loginPage");
+let passwordInputLogin = document.querySelector(".passwordInputLogin");
+let loginBtn = document.querySelector(".login-btn");
+let signUpPage = document.querySelector(".signUpPage");
+let passwordInputsignUp = document.querySelector(".passwordInputsignUp");
+let signUpBtn = document.querySelector(".signUp-btn");
+let favIcon = document.querySelector("link[rel='shortcut icon']");
+let containerOfNotes = document.querySelector(".notes");
+let secertBtn = document.querySelector(".secert-btn");
+let fakeNotes = document.querySelector(".fake-notes");
+let logOut = document.querySelector(".logout");
+let clickCounter = 0;
+let nextElementValue;
+
+
+
+// check localstorage if there is password stored or not
+let passcode = localStorage.getItem("passcode") || null;
+
+// in case there is password stored the login page display .. if there is no password the signup page display
+if (passcode) {
+  login();
+} else {
+  signUp();
+}
+
+// check localstorage if there are notes stored or not
+let notes = JSON.parse(localStorage.getItem("note"))
+  ? JSON.parse(localStorage.getItem("note"))
+  : [];
+// display notes if there are available notes
+displayNotes(notes);
+// create new note
+saveBtn.addEventListener("click", function () {
+  if (input.value) {
+    notes.push(input.value);
+    localStorage.setItem("note", JSON.stringify(notes));
+    input.value = "";
+    displayNotes(notes);
+  } else {
+    alert("Can't Add Empty Note");
+  }
+});
+
+function displayNotes(notes) {
+  containerOfNotes.innerHTML = "";
+  let card;
+  for (const note of notes) {
+    card = `<div class="note">
+    <i class='bx bx-edit editIcon edit'></i>
+    <div class="note-content">${note}</div>
+    <i class='bx bxs-message-square-x removeIcon delete'></i>
+    </div>`;
+    containerOfNotes.insertAdjacentHTML("beforeend", card);
+  }
+}
+
+containerOfNotes.addEventListener("click", function (e) {
+  //  delete a note
+  if (e.target.classList.contains("delete")) {
+    let userDecision = confirm("Are You Sure To Delete The Note");
+    if (userDecision) {
+      // remove from dom
+      e.target.parentElement.remove();
+      // remove from the array
+      notes.splice(
+        notes.findIndex(
+          (note) => note == e.target.previousElementSibling.textContent
+        ),
+        1
+      );
+      // remove from the localstorage
+      localStorage.setItem("note", JSON.stringify(notes));
+    }
+  }
+
+  //  edit a note
+  if (e.target.classList.contains("edit")) {
+    noteUpdaterBox.classList.remove("show-note-updater");
+    inputUpdate.focus();
+    inputUpdate.value = e.target.nextElementSibling.textContent;
+    nextElementValue = e.target.nextElementSibling.textContent;
+  }
+});
+
+updateBtn.addEventListener("click", function () {
+  if (inputUpdate.value != "") {
+    // update array
+    notes[notes.findIndex((note) => note == nextElementValue)] =
+      inputUpdate.value;
+    // update dom
+    inputUpdate.value = "";
+    displayNotes(notes);
+    noteUpdaterBox.classList.add("show-note-updater");
+    // update local storage
+    localStorage.setItem("note", JSON.stringify(notes));
+  } else {
+    alert("mom");
+  }
+});
+
+// unhide notes by the secret btn
+secertBtn.addEventListener("click", function () {
+  clickCounter++;
+  if (clickCounter == 5) {
+    fakeNotes.classList.remove("show-fake-notes");
+    containerOfNotes.style.display = "flex";
+  }
+});
+
+function login() {
+  loginPage.style.display = "flex";
+  loginBtn.addEventListener("click", function () {
+    if (passwordInputLogin.value == passcode) {
+      alert("logged in successfully");
+      favIcon.href = "./unlock.png";
+      loginPage.style.display = "none";
+      fakeNotes.classList.add("show-fake-notes");
+      // in case this the first visit to the site from this user
+      let firstVisit = JSON.parse(localStorage.getItem("firstVisit"));
+      setTimeout(() => {
+        if (firstVisit == null) {
+          alert("Show YOUR Hidden Notes By Clicking this 'Your Notes' 5 Times");
+          firstVisit = false;
+          localStorage.setItem("firstVisit", JSON.stringify(firstVisit));
+        }
+      }, 200);
+    } else {
+      alert("Wrong Password");
+    }
+    passwordInputLogin.value = "";
+  });
+}
+function signUp() {
+  signUpPage.style.display = "flex";
+  signUpBtn.addEventListener("click", function () {
+    if (passwordInputsignUp.value.length > 9) {
+      passcode = passwordInputsignUp.value;
+      localStorage.setItem("passcode", passcode);
+      alert("Sign up Successfully");
+      passwordInputsignUp.value = "";
+      signUpPage.style.display = "none";
+      loginPage.style.display = "flex";
+      location.reload();
+    } else {
+      alert("Password should be more than 9 Characters");
+      passwordInputsignUp.value = "";
+    }
+  });
+}
---
+++ readme.md
@@ -0,0 +1,39 @@
+# Only Notes  (A Simple Note-Taking App) 
+
+This is a readme file for Only Notes, a single-page web application that allows you to create, edit, and delete notes. It's designed to be a simple and user-friendly tool for managing your thoughts and ideas.
+
+# Demo:
+[Only Notes](https://only-notes.netlify.app/)
+
+## Features:
+* create Password to login to Your Notes Using it.
+* After Login, Fake Notes are displayed till you click the magic button 5 times so if someone has accessed your phone and he has Your Password and tried to log in using Your password he will see just the fake notes not the real ones (Unless he reached this repo and has an idea about the magic button LMFAO).
+* Add new notes with your thoughts and ideas.
+* Edit existing notes to keep them up-to-date.
+* Delete notes you no longer need.
+* Notes are saved locally in your browser's storage.
+
+## Technologies Used:
+
+* HTML: Provides the basic structure of the web page.
+* CSS: Styles the visual appearance of the application.
+* JavaScript: Enables interactivity and functionality like adding, editing, and deleting notes.
+
+## Deployment:
+
+This application is currently deployed on Netlify.
+
+## Getting Started:
+
+* Clone this repository or download the files.
+* Open the index.html file in your web browser.
+
+## Feedback:
+
+If you have any feedback or suggestions, please feel free to contact me.
+
+Thank you for using this program!
+
+## Contributing:
+
+If you would like to contribute to this program, please fork the repository and submit a pull request.
